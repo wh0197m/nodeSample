7600fe16e816e54d36634b61fd6f55011a5cde87
wh0197m <wh0197m@gmail.com>
2017-03-21 22:24:23 +0800

    Buffer

--- Buffer/Buffer.copy.js
+++ Buffer/Buffer.copy.js
@@ -3,6 +3,21 @@
  * @Author: wh01am
  * @Contact: wh0197m@gmail.com
  * @Last Modified By: wh01am
- * @Last Modified Time: Mar 17, 2017 10:53 PM
+ * @Last Modified Time: Mar 18, 2017 11:38 AM
  * @Description: Buffer拷贝
  */
+let buf1 = new Buffer(26);
+let buf2 = new Buffer(26);
+
+for (let i = 0; i < 26; i++) {
+    buf1[i] = i + 97;
+    buf2[i] = 97;
+}
+
+console.info(`${buf1}`);
+console.info(`${buf2}`);
+
+/* Node并不提供直接拷贝副本的方式方法 */
+buf1.copy(buf2, 0, 0, 5); //Buffer.copy(targeBuffer, targetStart, sourceStart, sourceEnd);
+console.info(`${buf1}`);
+console.info(`${buf2}`);
--- Buffer/Buffer.slice.js
+++ Buffer/Buffer.slice.js
@@ -3,6 +3,14 @@
  * @Author: wh01am
  * @Contact: wh0197m@gmail.com
  * @Last Modified By: wh01am
- * @Last Modified Time: Mar 17, 2017 10:52 PM
+ * @Last Modified Time: Mar 18, 2017 10:50 AM
  * @Description: Buffer裁剪
  */
+let buf = new Buffer(26);
+for (let i = 0; i < 26; i++) {
+    buf[i] = 97 + i;
+}
+let buf_slice_a = buf.slice(0, 5); //从位置0开始到位置5，直接切割，然后返回16进制内容
+console.info(buf_slice_a); // 打印<Buffer 61 62 63 64 65>
+console.info(`${buf}`);
+console.info(`buf_slice_a is: ${buf_slice_a}`); // 打印buf_slice_a is: abcde，因为有字符串连接，最终转换成了字符串
